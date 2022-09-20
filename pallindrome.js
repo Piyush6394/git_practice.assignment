81188ffececb0705da6b55f157cdd55b555d1b16
piyush6394 <piyushsingh639450@gmail.com>
2022-09-20 10:40:43 +0530

    pallindrome and primenum

---
+++ pallindrome.js
@@ -0,0 +1,8 @@
+function pallindrome(str){
+ for(let i=str.length-1;i>=0;i--){
+    console.log(str[i])
+ }
+}
+ str = "sanjay"
+pallindrome(str)
+ 
---
+++ prime_number.js
@@ -0,0 +1,9 @@
+function primenum(N){
+ for(le i=0;i<=N;i++){
+  if(N%N){
+    console.log("prime")
+  }else{
+    console.log("Not prime")
+ }
+}
+primenum(13)
